3d6cc543f52217b0e9c296bcef48471a37e7383b
IDO_PC\idoab <idoabadi6599@gmail.com>
2025-12-30 19:04:03 +0200

    initial commit

---
+++ server.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+
+IP = '0.0.0.0'
+PORT = 5555
+
+clients = {}
+
+def handle_client(client_socket, address):
+    username = None
+    try:
+        username = client_socket.recv(1024).decode('utf-8')
+        clients[username] = client_socket
+        print(f"[+] {username} connected from {address}")
+
+        while True:
+            data = client_socket.recv(1024).decode('utf-8')
+            if not data:
+                break
+            if ":" in data:
+                target, message = data.split(":", 1)
+                if target in clients:
+                    clients[target].send(f"from {username}: {message}".encode(utf-8))
+                else:
+                    client_socket.send(f"Error: User {target} not found.".encode('utf-8'))
+            
+    except Exception as e:
+        print(f"[!] error with {username}: {e}")
+
+    finally:
+        if username in clients:
+            del clients[username]
+            print(f"[-] {username} disconnected")
+            client_socket.close()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((IP, PORT))
+server.listen(5)
