799b052d454d6e124d7f251aebb807bbf11d9eae
ido abadi <idoabadi6599@gmail.com>
2026-01-08 17:15:21 +0200

    added ddos defence and client list + notification

--- server.py
+++ server.py
@@ -10,9 +10,23 @@ def handle_client(client_socket, address):
     username = None
     try:
         username = client_socket.recv(1024).decode('utf-8')
+        current_users = ", ".join(clients.keys())
+        if not current_users:
+            current_users = "None (You are the first!)"
+
         clients[username] = client_socket
         print(f"[+] {username} connected from {address}")
 
+        welcome_msg = f"Connected! Current users: {current_users}"
+        client_socket.send(welcome_msg.encode('utf-8'))
+        announcement = f"\n[Notification] {username} joined the chat!"
+        for name, sock in clients.items():
+            if name != username: 
+                try:
+                    sock.send(announcement.encode('utf-8'))
+                except:
+                    
+                    pass
         while True:
             data = client_socket.recv(1024).decode('utf-8')
             if not data:
@@ -34,11 +48,17 @@ def handle_client(client_socket, address):
             client_socket.close()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((IP, PORT))
-server.listen(5)
+server.listen(30)
 print(f"[*] server is running and listeninng on port {PORT}")
 
 while True:
     client_sock, addr = server.accept()
     # making a new process for each client
+    if len(clients) >= 30:
+        print(f"[!] Blocked connection from {addr}: Reached maximum clients (30)")
+        client_sock.send("Server is full. Try again later.".encode('utf-8'))
+        client_sock.close()
+        continue
+
     thread =  threading.Thread(target=handle_client, args=(client_sock, addr))
     thread.start()
